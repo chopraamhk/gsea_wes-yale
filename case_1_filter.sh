0edd17e897caa16032b225dd0f3dd357e54d1cad
Mehak Chopra <m.chopra1@universityofgalway.ie>
2025-04-03 12:43:35 +0100

    Update case_1_filter.sh

--- case_1_filter.sh
+++ case_1_filter.sh
@@ -16,9 +16,16 @@ bedtools intersect -v -a TAA_1278EURpts_AC0_VCF.vcf.gz -b target_regions_yale_we
 #to get only the targetted regions 
 bcftools view -R target_regions_yale_wes/xgen_plus_spikein.b38.bed target_regions_yale_wes/xgen_plus_spikein.b38.bed > target_TAA_1278EUR_200k.vcf
 bgzip target_TAA_1278EUR_200k.vcf
-tabix -p vcf target_TAA_1278EUR_200k.vcf
+tabix -p vcf target_TAA_1278EUR_200k.vcf.gz
 
 #if you want to know the number of variants in a specific vcf file 
 #bcftools view -H file.vcf | wc -l
 
 #-H -> it will get rid of the header
+
+#Since the file is already annotated by vep, we are going to filter the variants by 10e-4 maf (i.e., 0.001)
+bcftools view -i 'INFO/gnomad_af<0.001' target_TAA_1278EUR_200k.vcf.gz > gnomad_maf_filtered.vcf
+
+bgzip gnomad_maf_filtered.vcf
+tabix -p vcf gnomad_maf_filtered.vcf.gz
+
