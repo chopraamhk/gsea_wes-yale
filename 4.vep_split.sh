c0acf84dddb52b38140280f3867730291c4671fb
Mehak Chopra <m.chopra1@universityofgalway.ie>
2025-04-06 20:02:24 +0100

    Update 4.vep_split.sh

--- 4.vep_split.sh
+++ 4.vep_split.sh
@@ -20,24 +20,23 @@ bcftools view -e AC=0 ../ukb_wgs_gnomad_annotated.vcf.gz > ukb_wgs_target_ac0_gn
 bgzip ukb_wgs_target_ac0_gnomad.vcf
 tabix -p vcf ukb_wgs_target_ac0_gnomad.vcf.gz
 
+#you can avoid following 4 lines of code and can start without getting worst or canonical transcripts
 #splitting info fields so that we can extract by inputting info easily later
 bcftools +split-vep -p worst_ -a CSQ -c Allele,Consequence,IMPACT,SYMBOL,Gene,Feature_type,Feature,BIOTYPE,INTRON,HGVSc,HGVSp,cDNA_position,CDS_position,Protein_position,Amino_acids,Codons,Existing_variation,DISTANCE,STRAND,FLAGS,SYMBOL_SOURCE,HGNC_ID,CANONICAL,SOURCE,DOMAINS,LoFtool,gnomAD,gnomAD_AC,gnomAD_AN,gnomAD_AF -s worst ukb_wgs_target_ac0_gnomad.vcf.gz -o worst_output.vcf
-
 bcftools +split-vep -p canon_ -a CSQ -c Allele,Consequence,IMPACT,SYMBOL,Gene,Feature_type,Feature,BIOTYPE,INTRON,HGVSc,HGVSp,cDNA_position,CDS_position,Protein_position,Amino_acids,Codons,Existing_variation,DISTANCE,STRAND,FLAGS,SYMBOL_SOURCE,HGNC_ID,CANONICAL,SOURCE,DOMAINS,LoFtool,gnomAD,gnomAD_AC,gnomAD_AN,gnomAD_AF -s primary ukb_wgs_target_ac0_gnomad.vcf.gz -o canonical_output.vcf
-
 paste <(bcftools view worst_output.vcf) <(bcftools view canonical_output.vcf) > combined_worst_canonical_split_vep_results.vcf
 paste <(bcftools view worst_output.vcf) <(bcftools view canonical_output.vcf) > combined_worst_canonical_split_vep_results.tsv
 
 #OR JUST DO BELOW 
 bcftools +split-vep -a CSQ -c Allele,Consequence,IMPACT,SYMBOL,Gene,Feature_type,Feature,BIOTYPE,INTRON,HGVSc,HGVSp,cDNA_position,CDS_position,Protein_position,Amino_acids,Codons,Existing_variation,DISTANCE,STRAND,FLAGS,SYMBOL_SOURCE,HGNC_ID,CANONICAL,SOURCE,DOMAINS,LoFtool,gnomAD,gnomAD_AC,gnomAD_AN,gnomAD_AF -s primary ukb_wgs_target_ac0_gnomad.vcf.gz -o vep_split.vcf
 
-bgzip combined_worst_canonical_split_vep_ac0_results.vcf
-tabix -p vcf combined_worst_canonical_split_vep_ac0_results.vcf.gz
+bgzip vep_split.vcf
+tabix -p vcf vep_split.vcf.gz
 
-#removing variants if gnomad allele frequency is less than 0.0001.
-bcftools view -i 'INFO/gnomAD_AF<0.0001' combined_worst_canonical_split_vep_results.vcf.gz > ukb_wgs_gnomad_rare_filtered.vcf
+#removing variants if gnomad allele frequency is less than 0.0001. This might give an error because in info gnomAD_AF, type = string instead of float. this command expects to see number but is getting confused. 
+#bcftools view -i 'INFO/gnomAD_AF<0.0001' vep_split.vcf.gz > ukb_wgs_gnomad_rare_filtered.vcf
+
 #if you are seeing arithmatic opertaor error then 
-bcftools view -i 'INFO/gnomAD_AF+0.0<0.0001' vep_split.vcf > ukb_wgs_gnomad_rare_filtered.vcf
-
+bcftools view -i 'INFO/gnomAD_AF+0.0<0.0001' vep_split.vcf.gz > ukb_wgs_gnomad_rare_filtered.vcf
 bgzip ukb_wgs_gnomad_rare_filtered.vcf
 tabix -p vcf ukb_wgs_gnomad_rare_filtered.vcf.gz
